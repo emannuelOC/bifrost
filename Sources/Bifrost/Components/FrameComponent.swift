f42bfcf26118c35d82e23930aa49bdcc44309db7
Emannuel Carvalho <emannuel.carvalho@icloud.com>
2020-05-21 16:50:43 -0300

    Default frame component to be filled by child component

--- Sources/Bifrost/Components/FrameComponent.swift
+++ Sources/Bifrost/Components/FrameComponent.swift
@@ -90,44 +90,42 @@ private class FrameComponentView: UIView {
             let childView = try DynamicView.createView(dynamicsComponent: component, actionDelegate: delegate)
             self.addSubview(childView)
             
-            let gravity = component.properties?.first(where: { $0.name == "gravity" })?.value as? Gravity ?? Gravity()
-            
             childView.translatesAutoresizingMaskIntoConstraints = false
             
-            if component.type == "frame" {
-                childView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
-                childView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
-                childView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
-                childView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
-                return
-            }
-            
-            switch gravity.vertical {
-            case .bottom:
-                childView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
-                childView.topAnchor.constraint(greaterThanOrEqualTo: self.topAnchor).isActive = true
-            case .top:
-                childView.bottomAnchor.constraint(lessThanOrEqualTo: self.bottomAnchor).isActive = true
-                childView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
-            case .center:
-                childView.topAnchor.constraint(greaterThanOrEqualTo: self.topAnchor).isActive = true
-                childView.bottomAnchor.constraint(lessThanOrEqualTo: self.bottomAnchor).isActive = true
-                childView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
-            }
-            
-            switch gravity.horizontal {
-            case .left:
-                childView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
-                childView.rightAnchor.constraint(lessThanOrEqualTo: self.rightAnchor).isActive = true
-            case .right:
-                childView.leftAnchor.constraint(greaterThanOrEqualTo: self.leftAnchor).isActive = true
-                childView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
-            case .center:
-                childView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
-                childView.rightAnchor.constraint(lessThanOrEqualTo: self.rightAnchor).isActive = true
-                childView.leftAnchor.constraint(greaterThanOrEqualTo: self.leftAnchor).isActive = true
+            if let gravity = component.properties?.first(where: { $0.name == "gravity" })?.value as? Gravity {
+                switch gravity.vertical {
+                case .bottom:
+                    childView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+                    childView.topAnchor.constraint(greaterThanOrEqualTo: self.topAnchor).isActive = true
+                case .top:
+                    childView.bottomAnchor.constraint(lessThanOrEqualTo: self.bottomAnchor).isActive = true
+                    childView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+                case .center:
+                    childView.topAnchor.constraint(greaterThanOrEqualTo: self.topAnchor).isActive = true
+                    childView.bottomAnchor.constraint(lessThanOrEqualTo: self.bottomAnchor).isActive = true
+                    childView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+                }
                 
+                switch gravity.horizontal {
+                case .left:
+                    childView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+                    childView.rightAnchor.constraint(lessThanOrEqualTo: self.rightAnchor).isActive = true
+                case .right:
+                    childView.leftAnchor.constraint(greaterThanOrEqualTo: self.leftAnchor).isActive = true
+                    childView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+                case .center:
+                    childView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+                    childView.rightAnchor.constraint(lessThanOrEqualTo: self.rightAnchor).isActive = true
+                    childView.leftAnchor.constraint(greaterThanOrEqualTo: self.leftAnchor).isActive = true
+                    
+                }
+            } else {
+                childView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+                childView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+                childView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+                childView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
             }
+            
         }
     }
 }
